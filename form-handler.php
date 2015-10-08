c5be54cc0633245625a70f39b0dc099921f4e086
dis-zephyro <zephyro@yandex.ru>
2015-10-09 01:34:41 +0300

    v1

---
+++ form-handler.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $tel = $_POST['tel'];
+    $product = $_POST['product'];
+    $select = $_POST['select'];
+
+
+	
+    require 'class.phpmailer.php';
+    $thm = "Вам отправлено сообщение с " . $_SERVER['SERVER_NAME'];
+    $msg = "<strong>Имя:</strong> $name<br/>";
+    if(isset($tel)){$msg .= "<strong>Телефон:</strong> $tel <br/>";}
+    if(isset($product)){$msg .= "<strong>Продукт:</strong> $product <br/>";}
+    if(isset($select)){$msg .= "<strong>Продукт:</strong> $select <br/>";}
+
+
+    $mail = new PHPMailer();
+    $mail->From = 'noreply@'. $_SERVER['SERVER_NAME']; // от кого
+    $mail->FromName = $_SERVER['SERVER_NAME']; // от кого
+    $mail->AddAddress('zephyro@yandex.ru', ''); // кому - адрес, Имя
+
+    $mail->IsHTML(true); // выставляем формат письма HTML
+    $mail->Subject = $thm; // тема письма
+    $mail->Body = $msg;
+
+    if (!$mail->Send()) die('Mailer Error: ' . $mail->ErrorInfo);
+
+    header("Location: " . $_SERVER["PHP_SELF"]);
+    exit;
+}
+?>
--- js/main.js
+++ js/main.js
@@ -4,8 +4,38 @@ $(document).ready(function() {
         responsiveWidth: 768,
         navigation: true,
         navigationPosition: 'right',
-        anchors: ['firstPage', 'secondPage', '3rdPage'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage'],
         menu: '.navbar-nav',
         scrollingSpeed: 1000
+    });
+});
+
+
+$(document).ready(function() {
+
+    $('.btn-submit').click(function() {
+
+        $('body').find('form:not(this)').children('label').removeClass('red'); //удаление всех сообщение об ошибке(валидатора)
+        var answer = checkForm($(this).closest('form').get(0)); //ответ от валидатора
+        if(answer != false)
+        {
+            var $form = $(this).closest('form'),
+                name    =     $('input[name="name"]', $form).val(),
+                tel   =     $('input[name="phone"]', $form).val(),
+                product    =     $('input[name="product"]', $form).val();
+                select    =     $('select[name="select"]', $form).val();
+            console.log(name, tel, product, select);
+            $.ajax({
+                type: "POST",
+                url: "form-handler.php",
+                data: {name: name, tel: tel, product: product, select: select}
+            }).done(function(msg) {
+                console.log(name, tel, product, select);
+                $('form').find('input[type=text], textarea').val('');
+                console.log('удачно');
+                $('.bs-example-modal-sm').modal('hide');
+                $('.modal-done').modal('show');
+            });
+        }
     });
 });
