bed255f67699f47d69b47cbca10bd23c18daaf5d
dis-zephyro <zephyro@yandex.ru>
2015-10-09 22:48:41 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
 $(document).ready(function() {
-    $('#page').fullpage({
-        responsiveHeight: 710,
-        responsiveWidth: 768,
-        navigation: true,
-        navigationPosition: 'right',
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage', '5rdPage'],
-        menu: '.navbar-nav',
-        scrollingSpeed: 1000
-    });
+
+    // Анимация
+    var Android = navigator.userAgent.search(/Android/i);
+    var iPhone = navigator.userAgent.search(/iPhone/i);
+    var iPad = navigator.userAgent.search(/iPad/i);
+    if(Android != -1 || iPhone != -1 || iPad != -1) {
+
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/mobile.css" />'); //подключение файла mobile.css если мобильник
+
+        $('.topnav li a').click(function(){
+            var str=$(this).attr('href');
+            $.scrollTo(str, 500, {offset:-105});
+            return false;
+        });
+
+
+    } else {
+
+        $('head').append('<link rel="stylesheet" type="text/css" href="js/vendor/jquery.fullPage/jquery.fullPage.css" />'); //подключение файла jquery.fullPage.css если не мобильник
+
+        $('#page').fullpage({
+            responsiveHeight: 710,
+            responsiveWidth: 768,
+            navigation: true,
+            navigationPosition: 'right',
+            anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage', '5rdPage'],
+            menu: '.navbar-nav',
+            scrollingSpeed: 1000
+        });
+    }
 });
+
 
 
 $(document).ready(function() {
@@ -31,13 +53,60 @@ $(document).ready(function() {
                 data: {name: name, tel: tel, product: product, select: select}
             }).done(function(msg) {
                 console.log(name, tel, product, select);
-                $('form').find('input[type=text], textarea').val('');
+                //$('form').find('input[type=text], textarea').val('');
                 console.log('удачно');
                 $('.bs-example-modal-sm').modal('hide');
                 $('.modal-done').modal('show');
             });
         }
     });
+});
+
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-placeholder', {
+        width: 600,
+        height: 400,
+        videoId: 'XuQ7nfglhKE',
+        playerVars: {
+            color: 'white',
+            playlist: 'taJ60kskkns,FG0fTKAqZ5g'
+        },
+        events: {
+            onReady: initialize
+        }
+    });
+}
+
+
+function initialize(){
+
+    // Обновляем элементы управления и загружаем
+    updateTimerDisplay();
+    updateProgressBar();
+
+    // Сброс старого интервала
+    clearInterval(time_update_interval);
+
+    // Запускаем обновление таймера и дорожки проигрывания
+    // каждую секунду.
+    time_update_interval = setInterval(function () {
+        updateTimerDisplay();
+        updateProgressBar();
+    }, 1000)
+
+}
+
+$('.modal-lg').on('hidden.bs.modal', function (e) {
+    player.pauseVideo();
+});
+
+$('#pause').on('click', function () {
+    player.pauseVideo();
+});
+
+$('.modal-video').on('click', function () {
+    player.pauseVideo();
 });
 
 
@@ -52,5 +121,30 @@ function init () {
         controls: []
     });
 
+    myGeoObject = new ymaps.GeoObject({
+
+        geometry: {
+            type: "Point",
+            coordinates: [55.7647,37.7198]
+        },
+
+        properties: {
+            iconContent: '',
+            hintContent: ''
+        }
+    }, {
+        preset: 'islands#icon',
+        iconColor: '#3a1aff',
+        draggable: true
+    });
+
     myMap.behaviors.disable('scrollZoom');
+
+    myMap.geoObjects.add(myGeoObject)
 }
+
+
+
+$('.topnav li a').click(function(){
+    $('.navbar-collapse').removeClass('in');
+});
